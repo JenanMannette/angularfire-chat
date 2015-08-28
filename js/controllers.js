5915f8baa0128ab8b8f63895ca53d632a5f486f7
Jenan Mannette <jenanmannette@gmail.com>
2015-08-28 15:12:03 -0600

    works

--- js/controllers.js
+++ js/controllers.js
@@ -1,6 +1,7 @@
 app.controller("ChatBox", function ($scope, $firebaseArray) {
  var logs = new Firebase('https://jenan-chat.firebaseio.com/chatlog');
     $scope.chatLogs = $firebaseArray(logs);
+    $scope.boolean = true;
 
     $scope.submitPost = function () {
         $scope.chatLogs.$add({
@@ -11,9 +12,17 @@ app.controller("ChatBox", function ($scope, $firebaseArray) {
         $scope.username = '';
         $scope.url = '';
         $scope.message = '';
-    }
+    };
 
     $scope.removePost = function (post) {
         $scope.chatLogs.$remove(post)
-    }
+    };
+
+    $scope.editPost = function () {
+        $scope.boolean = false;
+    };
+
+    $scope.updatePost = function () {
+        $scope.boolean = true;
+    };
 });
